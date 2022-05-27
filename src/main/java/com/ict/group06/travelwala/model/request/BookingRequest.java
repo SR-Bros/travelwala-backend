12652dc249764f8ecc999673eefa514a9fa58927
Hieu Tran <hieutt6@sapo.vn>
2022-05-27 09:03:41 +0700

    chore: add ContactRequest to BookingRequest

--- src/main/java/com/ict/group06/travelwala/model/request/BookingRequest.java
+++ src/main/java/com/ict/group06/travelwala/model/request/BookingRequest.java
@@ -9,4 +9,5 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class BookingRequest {
     private FlightBookingRequest flightBookingRequest;
+    private ContactRequest contactRequest;
 }
