1df0b9f611bc56483a1486e655bde7e8157a178e
Hieu Tran <hieutt6@sapo.vn>
2022-06-08 20:29:39 +0700

    chore: add some exceptions

---
+++ src/main/java/com/ict/group06/travelwala/exception/FlightLocationException.java
@@ -0,0 +1,7 @@
+package com.ict.group06.travelwala.exception;
+
+public class FlightLocationException extends RuntimeException {
+    public FlightLocationException(String message) {
+        super(message);
+    }
+}
