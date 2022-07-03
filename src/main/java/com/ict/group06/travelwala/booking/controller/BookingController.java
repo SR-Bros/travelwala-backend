369e26c10a0f99315cef3c5251519171d7931eb7
Hieu Tran <hieu.trantrung2908@gmail.com>
2022-07-03 16:08:17 +0700

    feature: implement api create booking

--- src/main/java/com/ict/group06/travelwala/booking/controller/BookingController.java
+++ src/main/java/com/ict/group06/travelwala/booking/controller/BookingController.java
@@ -1,6 +1,8 @@
 package com.ict.group06.travelwala.booking.controller;
 
+import com.ict.group06.travelwala.booking.service.ICreateBooking;
 import com.ict.group06.travelwala.model.request.CreateBookingRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,11 +11,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/bookings")
 public class BookingController {
+    private final ICreateBooking createBooking;
+
     @PostMapping()
     public ResponseEntity<?> book(@Valid @RequestBody CreateBookingRequest bookingRequest) {
-        return null;
+        return ResponseEntity.ok().body(createBooking.createBooking(bookingRequest));
     }
 }
